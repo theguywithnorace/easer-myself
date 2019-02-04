ec210b602de46af72af98eb8ab4ef17d8bedc163
timotheein <easer.ceo@gmail.com>
2019-02-04 15:56:58 +0100

    Succes of the Facebook connection !!! via firebase
    ...at last

--- app/src/main/java/com/example/easermyself/Login.java
+++ app/src/main/java/com/example/easermyself/Login.java
@@ -1,27 +1,73 @@
 package com.example.easermyself;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.easermyself.Registering.PhoneNumberAsked;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
-import java.util.List;
+
+import butterknife.ButterKnife;
 
 public class Login extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Remove title bar
+
         setContentView(R.layout.login);
+        Button connexionButton = findViewById(R.id.connexionButton);
+
+        ButterKnife.bind(this);
+
+
+        connexionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(
+                                        Arrays.asList(
+                                                new AuthUI.IdpConfig.FacebookBuilder().build())
+                                )
+                                .build(),
+                        RC_SIGN_IN);
+
+            }
+        });
+
     }
 
-    //Connexion via facebook
-    AuthUI.IdpConfig provider = new AuthUI.IdpConfig.FacebookBuilder().build();
 
-    // Create and launch sign-in intent
-  //
+    // Result on connection
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                Intent intent = new Intent(Login.this, PhoneNumberAsked.class);
+                startActivity(intent);
+            }
+        }
+    }
+
+
+
+
 }
