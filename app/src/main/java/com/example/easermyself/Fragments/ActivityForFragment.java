9e84e25199b48398a0e5f0618bdc339e4eaab2ac
timotheein <easer.ceo@gmail.com>
2019-02-08 15:30:18 +0100

    Le main viewpager est en place, avec une toolbar individuelle par page.
    sur l'écran contact on peut acceder à son profil à partir d'un bouton.
    
    Trying to end the Fragment tutorial
    
    //à faire :
    Le top serait que ce bouton MyProfile soit dans la toolbar !!

--- app/src/main/java/com/example/easermyself/Fragments/ActivityForFragment.java
+++ app/src/main/java/com/example/easermyself/Fragments/ActivityForFragment.java
@@ -1,6 +1,7 @@
-package com.example.easermyself.Framents;
+package com.example.easermyself.Fragments;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
@@ -8,14 +9,31 @@ import com.example.easermyself.R;
 import com.example.easermyself.Base.BaseActivity;
 
 public class ActivityForFragment extends BaseActivity implements MyFragment.OnButtonClickedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_for_fragment);
+
+    }
+
     @Override
     public int getFragmentLayout() {
         return R.layout.activity_for_fragment;
     }
+
     @Override
     public void onButtonClicked(View view) {
+
+
         Log.e(getClass().getSimpleName(),"Button clicked !");
-        Intent intent = new Intent(ActivityForFragment.this, ActivityForFragmentDetails.class);
-        startActivity(intent);
+
+
+        int buttonTag = Integer.parseInt(view.getTag().toString());
+
+         Intent i = new Intent(this, MyFragmentDetails.class);
+            i.putExtra(ActivityForFragmentDetails.EXTRA_BUTTON_TAG, buttonTag);
+            startActivity(i);
+
     }
 }
